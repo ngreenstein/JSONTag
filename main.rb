47092fe275619c6ae2010ec438432158283f5e0e
Nathan Greenstein <nathang345@gmail.com>
2012-06-10 22:38:39 -0700

    Added checks to only set properties specified in the JSON file. Added path to artwork as property in JSON.

--- main.rb
+++ main.rb
@@ -15,21 +15,22 @@ TagLib::MPEG::File.open("sample copy.mp3") do |file|
 	tag = file.id3v2_tag(true)
 
 	# Set attributes
-	 tag.title = attributes['title']
-	tag.artist = attributes['artist']
-	 tag.album = attributes['album']
-	  tag.year = attributes['year']
-	 tag.track = attributes['track_number']
-	 tag.genre = attributes['genre']
+	if attributes['title'] 				then  tag.title = attributes['title']				 end
+	if attributes['artist'] 			then tag.artist = attributes['artist']			 end
+	if attributes['album'] 				then  tag.album = attributes['album']				 end
+	if attributes['year'] 				then   tag.year = attributes['year']				 end
+	if attributes['track_number'] then  tag.track = attributes['track_number'] end
+	if attributes['genre'] 				then  tag.genre = attributes['genre']				 end
 	 
 	 #Add artwork
-	 artframe = TagLib::ID3v2::AttachedPictureFrame.new
-	 artframe.mime_type = "image/png"
-	 artframe.description = "Cover"
-	 artframe.type = TagLib::ID3v2::AttachedPictureFrame::FrontCover
-	 artframe.picture = File.open("sample.png", 'rb') { |f| f.read }
-	 
-	 tag.add_frame(artframe)
+	 if attributes['artwork']
+		 artframe = TagLib::ID3v2::AttachedPictureFrame.new
+		 artframe.mime_type = "image/png"
+		 artframe.description = "Cover"
+		 artframe.type = TagLib::ID3v2::AttachedPictureFrame::FrontCover
+		 artframe.picture = File.open(attributes['artwork'], 'rb') { |f| f.read }
+		 tag.add_frame(artframe)
+	 end
 
 	#Save file
 	file.save
